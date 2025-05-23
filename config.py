1b03e4bf96b8183aae87db9c5e591291d2f0d76a
Tharani <tharani.k@mresult.com>
2025-05-23 12:31:23 +0530

    configurations\ File

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
-
-# File path configuration
-FILE_PATH = os.path.abspath("Data/aa_dataset-tickets-multi-lang-5-2-50-version.csv")
-
-# Stopwords setup
 import nltk
 from nltk.corpus import stopwords
+
+
+FILE_PATH = os.path.abspath("Data/aa_dataset-tickets-multi-lang-5-2-50-version.csv")
+
+
 nltk.download("stopwords")
 STOPWORDS = set(stopwords.words("english"))
